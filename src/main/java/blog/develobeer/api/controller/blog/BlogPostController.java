b272e7e851b25301f6accf37e98f9827b40291ec
kokj <kokj@neowiz.com>
2018-10-26 16:30:40 +0900

    test용 CrossOrigin 추가

--- src/main/java/blog/develobeer/api/controller/blog/BlogPostController.java
+++ src/main/java/blog/develobeer/api/controller/blog/BlogPostController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 @RequestMapping("/post")
 public class BlogPostController {
 
