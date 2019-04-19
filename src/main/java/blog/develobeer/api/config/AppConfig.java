33532b4679ceb891cfa37f9631d970c43b162ed4
kokj <kokj@neowiz.com>
2019-04-19 13:23:28 +0900

    CORS 이슈 수정

--- src/main/java/blog/develobeer/api/config/AppConfig.java
+++ src/main/java/blog/develobeer/api/config/AppConfig.java
@@ -1,6 +1,7 @@
 package blog.develobeer.api.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +15,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Data
 public class AppConfig implements WebMvcConfigurer {
-    private String[] hosts;
+    @Value("${spring.origin}")
+    private String allowOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/info/**")
-                .allowedOrigins(hosts)
+        registry.addMapping("/**")
+                .allowedOrigins(allowOrigin)
                 .allowedMethods("POST", "GET",  "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("X-Auth-Token", "Content-Type")
+                .allowedHeaders("X-Auth-Token", "Content-Type", "Access-Control-Allow-Origin")
 //                .exposedHeaders("custom-header1", "custom-header2")
                 .allowCredentials(false)
                 .maxAge(4800);
