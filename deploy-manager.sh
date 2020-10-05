ba2813500a9b3f50a1a7a2df4e20bd209944a675
sky6154 <sky6154@gmail.com>
2020-10-05 14:58:18 +0900

    docker stack deploy 방식으로 배포 변경

--- deploy-manager.sh
+++ deploy-manager.sh
@@ -6,7 +6,7 @@
 {
   echo try to deploy docker stack...
   # -e 옵션이 지원되지 않기 때문에, .env 파일을 생성하기 보다 도커 명령어 실행 전에 앞에 변수를 넘기면, .env 파일처럼 사용 가능하다.
-  TAG=$1 docker stack deploy --prune --compose-file docker-compose.yml blogApi
+  TAG=$1 docker stack deploy --with-registry-auth --prune --compose-file docker-compose.yml blogApi
 }
 
 #docker service create --name blog-api --replicas 2 --publish 80:80 blog-api:latest
